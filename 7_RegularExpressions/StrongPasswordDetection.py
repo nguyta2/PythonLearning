3101ae38173de9d472cd3603c05ecb2ffd8fc4d3
nguyta2 <nttamspkt@gmail.com>
2021-09-01 20:13:36 +0700

    Update StrongPasswordDetection

--- 7_RegularExpressions/StrongPasswordDetection.py
+++ 7_RegularExpressions/StrongPasswordDetection.py
@@ -1,1 +1,32 @@
 import re
+
+passWord = input('Enter your password:')
+
+def atLeastOneUpperCase(inPassWord):
+    if(re.search('[A-Z]', inPassWord)):
+        return True
+    else:
+        return False
+
+def atLeastOneLowerCase(inPassWord):
+    if(re.search('[a-z]', inPassWord)):
+        return True
+    else:
+        return False
+
+def atLeastOneDigit(inPassWord):
+    if(re.search('\d', inPassWord)):
+        return True
+    else:
+        return False
+
+if(len(passWord) >= 8 and atLeastOneUpperCase(passWord) and atLeastOneLowerCase(passWord)
+and atLeastOneDigit(passWord)):
+    print('This password is STRONG')
+else:
+    print('This password is NOT STRONG')
+
+# if mo.group() == passWordRegex:
+#     print('The password is STRONG')
+# else:
+#     print('The password is not STRONG', 'You need to change it')
